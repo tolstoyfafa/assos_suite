7999345c4b2bf0b123bcf033099a278d17074c35
Fayçal BORDJAH <fbordjah@linagora.com>
2020-02-26 20:54:33 +0100

    :tada: init the backend and frontend

---
+++ backend/src/main/java/com/motus/assosuite/AssosuiteApplication.java
@@ -0,0 +1,13 @@
+package com.motus.assosuite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssosuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssosuiteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/motus/assosuite/AssosuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motus.assosuite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssosuiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
