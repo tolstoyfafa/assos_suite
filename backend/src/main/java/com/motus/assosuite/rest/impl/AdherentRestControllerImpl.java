85f65f5518d190d000378114d0090bece7b573c6
Fayçal BORDJAH <fbordjah@linagora.com>
2020-03-14 16:54:16 +0100

    :sparkles: Rest Endpoint to add adherent

--- backend/src/main/java/com/motus/assosuite/rest/impl/AdherentRestControllerImpl.java
+++ backend/src/main/java/com/motus/assosuite/rest/impl/AdherentRestControllerImpl.java
@@ -1,5 +1,28 @@
 package com.motus.assosuite.rest.impl;
 
-public class AdherentRestControllerImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.motus.assosuite.models.Adherent;
+import com.motus.assosuite.rest.AdherentRestController;
+import com.motus.assosuite.service.AdherentService;
+
+@RestController(value = "/assosuite/api/adherents")
+public class AdherentRestControllerImpl implements AdherentRestController {
+
+	private final AdherentService service;
+
+	@Autowired
+	public AdherentRestControllerImpl(AdherentService service) {
+		this.service = service;
+	}
+
+	@PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE }, value = "/")
+	@Override
+	public Adherent create(@RequestBody Adherent adherent) {
+		return service.create(adherent);
+	}
 }
