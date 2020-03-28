b4b3e976a3fa39cea73bfbcdd79351d79e36e077
Fayçal BORDJAH <fbordjah@linagora.com>
2020-03-28 18:02:18 +0100

    :sparkles: Add findAll endpoint for adherent

--- backend/src/main/java/com/motus/assosuite/service/impl/AdherentServiceImpl.java
+++ backend/src/main/java/com/motus/assosuite/service/impl/AdherentServiceImpl.java
@@ -1,5 +1,7 @@
 package com.motus.assosuite.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,18 +10,23 @@ import com.motus.assosuite.repository.AdherentRepository;
 import com.motus.assosuite.service.AdherentService;
 
 @Service
-public class AdherentServiceImpl implements AdherentService{
-	
+public class AdherentServiceImpl implements AdherentService {
+
 	private final AdherentRepository repository;
 
 	@Autowired
 	public AdherentServiceImpl(AdherentRepository repository) {
 		this.repository = repository;
 	}
-	
+
 	@Override
 	public Adherent create(Adherent adherent) {
 		return repository.save(adherent);
+	}
+
+	@Override
+	public List<Adherent> findAll() {
+		return repository.findAll();
 	}
 
 }
