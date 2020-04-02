cdf882f598d3713a92f2f1d7c5fc78873a26a645
Fayçal BORDJAH <fbordjah@linagora.com>
2020-04-02 15:58:20 +0200

    :sparkles: Enable web security

--- backend/src/main/java/com/motus/assosuite/config/SecurityConfiguration.java
+++ backend/src/main/java/com/motus/assosuite/config/SecurityConfiguration.java
@@ -3,10 +3,13 @@ package com.motus.assosuite.config;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 @EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
 //TODO: If actuator enabled add this @SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
-public class SecurityConfiguration {
-
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 }
