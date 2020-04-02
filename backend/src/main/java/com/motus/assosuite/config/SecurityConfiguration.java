ef66e33d3cce900a31e8215cbb591bb875cecb70
Fayçal BORDJAH <fbordjah@linagora.com>
2020-04-02 20:56:43 +0200

    :sparkles: Add Bcrypt bean to encode passwords

--- backend/src/main/java/com/motus/assosuite/config/SecurityConfiguration.java
+++ backend/src/main/java/com/motus/assosuite/config/SecurityConfiguration.java
@@ -2,14 +2,21 @@ package com.motus.assosuite.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
 //TODO: If actuator enabled add this @SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
