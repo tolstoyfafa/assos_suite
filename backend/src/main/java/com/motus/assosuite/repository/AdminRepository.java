701f3c799b0b47aaac4aacc6bfff153302a8d2e5
Fayçal BORDJAH <fbordjah@linagora.com>
2020-04-03 17:43:56 +0200

    :sparkles: ADd getDefault to recover default admin for tests

--- backend/src/main/java/com/motus/assosuite/repository/AdminRepository.java
+++ backend/src/main/java/com/motus/assosuite/repository/AdminRepository.java
@@ -1,10 +1,13 @@
 package com.motus.assosuite.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.motus.assosuite.models.Admin;
 
 public interface AdminRepository extends MongoRepository<Admin, String> {
+	@Query("{'mail': 'adminstrator@assosuite.com'}")
+	Admin getDefault();
 
 	Admin findByUuid(String uuid);
 
