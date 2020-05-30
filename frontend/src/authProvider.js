63171658cae01715f990d4d74bb5aa9e0c6e4090
Fayçal BORDJAH <fbordjah@linagora.com>
2020-05-30 17:06:48 +0200

    :spreakless: [FRONT] authProvider & dataProvider

---
+++ frontend/src/authProvider.js
@@ -0,0 +1,47 @@
+import { BACK_URL } from "./constants";
+import decodeJwt from 'jwt-decode'
+
+
+const auth_url = BACK_URL.concat("/authentication/authenticate")
+export default {
+    login: (opts) => {
+        const request = new Request(auth_url, {
+            method: 'POST',
+            body: JSON.stringify({ 
+                mail: opts.username,
+                password: opts.password }),
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+        });
+        console.log(request.body)
+        return fetch(request)
+            .then(response => {
+
+                if (response.status < 200 || response.status >= 300) {
+                    throw new Error(response.message);
+                }
+                return response.json();
+            })
+            .then(({ token }) => {
+                const decodedToken = decodeJwt(token);
+
+                localStorage.setItem('token', token);
+                localStorage.setItem('permissions', decodedToken.roles[0].authority);
+                console.log(decodedToken.roles)
+            });
+    },
+    logout: () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('permissions');
+        return Promise.resolve();
+    },
+    checkError: error => {
+        console.log(error)
+    },
+    checkAuth: () => {
+        return localStorage.getItem('token') ? Promise.resolve() : Promise.reject();
+    },
+    getPermissions: () => {
+        const role = localStorage.getItem('permissions');
+        return role ? Promise.resolve(role) : Promise.reject();
+    }
+};
