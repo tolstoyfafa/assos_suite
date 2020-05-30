4c48872cf77a2e6f95e69e20cb12eebd032b1fc2
Fayçal BORDJAH <fbordjah@linagora.com>
2020-05-30 18:24:36 +0200

    :sparkles: [FRONT] add authorization firewell && AdherentList Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { Admin, Resource,ListGuesser, EditGuesser} from 'react-admin';
+import { Admin, Resource} from 'react-admin';
 import dataProvider from './dataProvider'
 import authProvider from './authProvider'
+import {AdherentList} from './AdherentList'
 import "./App.css";
 import {BACK_URL}  from "./constants";
+import { httpClient } from "./authorization";
 
 const App = () => (
-  <Admin dataProvider={dataProvider(BACK_URL)} authProvider={authProvider} ></Admin>
+  <Admin title = "AssoSuite"
+        dataProvider={dataProvider(BACK_URL, httpClient)}
+        authProvider={authProvider} >
+    <Resource 
+        name="adherents"
+        title="ADHERENTS"
+        list={AdherentList} 
+        >
+    </Resource>
+  </Admin>
+
 )
 
 export default App;
