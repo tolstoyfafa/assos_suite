72c201b911cc33af2a0bd0cc5df9753076f5cdef
Fayçal BORDJAH <fbordjah@linagora.com>
2020-05-31 14:30:21 +0200

    :sprakles: [FRONT] Add create adherent view

---
+++ frontend/src/AdherentCreate.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Create, Edit, SimpleForm, TextInput, DateInput, SelectInput,Toolbar,SaveButton, Datagrid, TextField, DateField, EditButton ,
+    required,
+    minLength,
+    maxLength,
+    minValue,
+    maxValue,
+    number,
+    regex,
+    email,
+    choices} from 'react-admin';
+
+const validateFirstName = [required("Le Nom est obligatoire"), minLength(2), maxLength(30)];
+const validateEmail = [required("Le Prénom est obligatoire"),email()];
+const validateZipCode = regex(/^\d{5}$/, 'Veuillez saisir un code postal valide');
+
+
+const AdhrentCreateToolbar = (props) => (<Toolbar {...props} >
+    <SaveButton
+        label="Ajouter"
+    />
+</Toolbar>);
+
+export const AdherentCreate = (props) => (
+    <Create {...props} title="Ajouter un adhérent" >
+    <SimpleForm toolbar={<AdhrentCreateToolbar/>} >
+        <TextInput label="Nom" source="firstName" validate={validateFirstName}/>
+        <TextInput label="Prénom" source="lastName" validate={validateFirstName} />
+        <TextInput label="Couriel" source="mail" type="email" validate={validateEmail} />
+        <TextInput label="Tél" source="phone" validate={[required("Le numéro de téléphone est obligatoire"),
+                     number(), minLength(2), maxLength(15)]} />
+        <TextInput label="numéro de la rue" source="address.streetNum" validate={[required(), number()]} />
+        <TextInput label="rue" source="address.street" validate={[required(), number()]} />
+        <TextInput label="ville" source="address.city" validate={[required("Le nom de la ville est obligatoire")]} />
+        <TextInput label="code postale" source="address.zip" validate={validateZipCode} />
+        <TextInput label="supplément addresse" source="address.description" />
+        <SelectInput label="sex" source="genderType" choices={[
+            { id: 'MALE', name: 'Homme'},
+            { id: 'FEMALE', name: 'Femme'},
+        ]}  helperText={""}/>
+        <SelectInput label="situation matrimoniale" source="familialSituation" choices={[
+            { id: 'SINGLE', name: 'Single'},
+            { id: 'MARIED', name: 'Maried'},
+        ]}  helperText={""}/>
+        <DateInput label="Naissance" source="birthDay"  validate={required()}/>
+    </SimpleForm>
+</Create>
+)
+
+    
+
+
