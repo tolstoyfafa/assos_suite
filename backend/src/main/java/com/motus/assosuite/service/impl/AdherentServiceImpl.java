2bcc8cf6174d9abf6ad186e4994479fb4c04caa8
Fayçal BORDJAH <fbordjah@linagora.com>
2020-06-01 14:05:06 +0200

    :sparkles: [BACK] Add MailServiceto send emails when user is added

--- backend/src/main/java/com/motus/assosuite/service/impl/AdherentServiceImpl.java
+++ backend/src/main/java/com/motus/assosuite/service/impl/AdherentServiceImpl.java
@@ -1,11 +1,15 @@
 package com.motus.assosuite.service.impl;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
+
+import javax.mail.MessagingException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -17,21 +21,35 @@ import com.motus.assosuite.enums.AssosBusinessErrorCode;
 import com.motus.assosuite.models.Adherent;
 import com.motus.assosuite.repository.AdherentRepository;
 import com.motus.assosuite.service.AdherentService;
+import com.motus.assosuite.service.MailService;
 
 @Service
 public class AdherentServiceImpl implements AdherentService {
 
 	private final AdherentRepository repository;
 	
+	private final MailService mailService;
+	
 	private static final Logger LOGGER = LoggerFactory.getLogger(AdherentServiceImpl.class);
 
 	@Autowired
-	public AdherentServiceImpl(AdherentRepository repository) {
+	public AdherentServiceImpl(AdherentRepository repository,
+			MailService mailSender) {
 		this.repository = repository;
+		this.mailService = mailSender;
 	}
 
 	@Override
 	public Adherent create(Adherent adherent) {
+		try {
+			LOGGER.info("Sending Mail to {}", adherent.getMail());
+			mailService.sendMailOnAdd(adherent.getMail());
+		} catch (MessagingException e) {
+			LOGGER.error("Error while sending an email",e);
+			e.printStackTrace();
+		}catch (IOException e) {
+            e.printStackTrace();
+        }
 		return repository.save(adherent);
 	}
 
