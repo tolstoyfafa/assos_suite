50e7f8af0adc4dfdd115152420f6f4dd51e85c84
Fayçal BORDJAH <fbordjah@linagora.com>
2020-06-01 15:24:29 +0200

    :sprakles: [FRONT] Add edit show delete button to the displayed list

--- frontend/src/AdherentList.js
+++ frontend/src/AdherentList.js
@@ -1,13 +1,16 @@
 import * as React from "react";
-import { List, Datagrid, TextField, EmailField } from 'react-admin';
+import { List, Datagrid, TextField, EmailField, EditButton,ShowButton,DeleteButton } from 'react-admin';
 
 export const AdherentList = props => (
-    <List {...props} >
-        <Datagrid rowClick="edit">
+    <List {...props} perPage={5} >
+        <Datagrid rowClick="show">
             <TextField source="firstName" />
             <TextField source="lastName" />
             <EmailField source="mail" />
             <TextField source="phone" />
+            <EditButton label="Modifier"/>
+            <ShowButton label="Détails"/>
+            <DeleteButton label="supprimer"/>
         </Datagrid>
     </List>
 );
