a30872de8329587534416975c0be9768e3f8483b
Fayçal BORDJAH <fbordjah@linagora.com>
2020-06-26 11:18:00 +0200

    :sparkles: Activate translation to french

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Admin, Resource} from 'react-admin';
+import polyglotI18nProvider from 'ra-i18n-polyglot';
+import frenchMessages from 'ra-language-french';
 import dataProvider from './dataProvider'
 import authProvider from './authProvider'
 import {AdherentList} from './AdherentList'
@@ -10,10 +12,12 @@ import { AdherentCreate } from "./AdherentCreate";
 import {AdherentEdit} from './AdherentEdit'
 import { AdherentShow } from "./AdhrentShow";
 
+const i18nProvider = polyglotI18nProvider(() => frenchMessages, 'fr');
 const App = () => (
   <Admin title = "AssoSuite"
         dataProvider={dataProvider(BACK_URL, httpClient)}
-        authProvider={authProvider} >
+        authProvider={authProvider}
+        i18nProvider={i18nProvider} >
     <Resource 
         name="adherents"
         title="ADHERENTS"
