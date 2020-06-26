85f95c7b0505098a086263ef422407e662888379
Fayçal BORDJAH <fbordjah@linagora.com>
2020-06-26 15:59:40 +0200

    :fix: Throw internal error when can't send mail due to credentials configuration

--- backend/src/main/java/com/motus/assosuite/enums/AssosBusinessErrorCode.java
+++ backend/src/main/java/com/motus/assosuite/enums/AssosBusinessErrorCode.java
@@ -7,7 +7,8 @@ public enum AssosBusinessErrorCode {
 	ADHERENT_NOT_FOUND(1000, HttpStatus.NOT_FOUND),
 	ADHERENT_EXISTS(1001, HttpStatus.BAD_REQUEST),
 	ADMIN_BAD_CREDENTIALS(2001, HttpStatus.UNAUTHORIZED),
-	ADMIN_NOT_FOUND(2002, HttpStatus.NOT_FOUND);
+	ADMIN_NOT_FOUND(2002, HttpStatus.NOT_FOUND),
+	MAIL_SEND_ERROR(3002, HttpStatus.INTERNAL_SERVER_ERROR);
 	
 	private final int code;
 	private final HttpStatus status;
