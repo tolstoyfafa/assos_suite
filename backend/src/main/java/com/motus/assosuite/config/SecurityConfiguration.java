64e7ed9cd5902cda0dbf4d913b9cb0f63d2e9b46
Fayçal BORDJAH <fbordjah@linagora.com>
2020-06-27 12:15:24 +0200

    :sparkles: [BACK] Add content-range header to findAll endpoint

--- backend/src/main/java/com/motus/assosuite/config/SecurityConfiguration.java
+++ backend/src/main/java/com/motus/assosuite/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.motus.assosuite.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
