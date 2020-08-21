3a752ea1970e95d8e5751b4f220725ede6ba1d7d
Fayçal BORDJAH <fbordjah@linagora.com>
2020-08-21 15:05:27 +0200

    :sparkles: [BACK] Add Group BECode

--- backend/src/main/java/com/motus/assosuite/enums/AssosBusinessErrorCode.java
+++ backend/src/main/java/com/motus/assosuite/enums/AssosBusinessErrorCode.java
@@ -2,24 +2,26 @@ package com.motus.assosuite.enums;
 
 import org.springframework.http.HttpStatus;
 
+/**
+ * Enum Contains all business error codes used to normalize the http response of the API
+ * @author fbordjah
+ *
+ */
 public enum AssosBusinessErrorCode {
 	
 	ADHERENT_NOT_FOUND(1000, HttpStatus.NOT_FOUND),
 	ADHERENT_EXISTS(1001, HttpStatus.BAD_REQUEST),
 	ADMIN_BAD_CREDENTIALS(2001, HttpStatus.UNAUTHORIZED),
-	ADMIN_NOT_FOUND(2002, HttpStatus.NOT_FOUND);
-	
+	ADMIN_NOT_FOUND(2002, HttpStatus.NOT_FOUND),
+	GROUP_NOT_FOUND(3000, HttpStatus.NOT_FOUND),
+	GROUP_EXISTS(3001, HttpStatus.BAD_REQUEST);
+
 	private final int code;
 	private final HttpStatus status;
 
 	private AssosBusinessErrorCode(int code, HttpStatus status) {
 		this.code = code;
 		this.status = status;
-	}
-	
-	private AssosBusinessErrorCode(int code) {
-		this.code = code;
-		this.status = HttpStatus.BAD_REQUEST;
 	}
 
 	public int getCode() {
