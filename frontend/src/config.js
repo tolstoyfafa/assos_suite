736f932ea834555fb48a4b2b7da426a68b4760f4
Fayçal BORDJAH <fbordjah@linagora.com>
2020-08-30 12:09:20 +0200

    :bug: [FRONT] fix env variable not readded on dockerfile

--- frontend/src/config.js
+++
@@ -1,6 +0,0 @@
-const settings = {
-    APP_HOST: window.REACT_APP_API_URL,
-    PORT: window.REACT_APP_PORT,
-  };
-  
-export default settings;
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,1 @@
-import settings from "./config";
-
-export const BACK_URL = "http://" + settings.APP_HOST + ":" + settings.PORT +"/assosuite/api/v1"
+export const BACK_URL = "http://" + window.REACT_APP_API_URL + ":" + window.REACT_APP_API_PORT +"/assosuite/api/v1"
