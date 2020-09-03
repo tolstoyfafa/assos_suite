a62131d675e1bdc055c51f4a6831dac2948bfb77
Fayçal BORDJAH <fbordjah@linagora.com>
2020-09-03 20:28:42 +0200

    :bug: [FRONT] remove API_PORT from constatnts

--- frontend/generate_config_js.sh
+++ frontend/generate_config_js.sh
@@ -3,6 +3,5 @@
 #echo -e "window.REACT_APP_API_URL='${API_URL}';\n window.REACT_APP_PORT='{API_PORT}'";
 cat <<EOF
 window.REACT_APP_API_URL='${API_URL}';
-window.REACT_APP_API_PORT='${API_PORT}';
 EOF
 
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,1 +1,1 @@
-export const BACK_URL = "https://" + window.REACT_APP_API_URL + ":" + window.REACT_APP_API_PORT +"/assosuite/api/v1"
+export const BACK_URL = "https://" + window.REACT_APP_API_URL + "/assosuite/api/v1"
